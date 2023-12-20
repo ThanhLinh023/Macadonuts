cc06a59d523e8b5b50b9fa35d8b4ec1390cb6b35
Nguyen Minh Phuc <nguyenminhphuc010603@gmail.com>
2023-12-20 08:30:29 +0700

    fix voucher layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,84 +33,86 @@
             </div>
         </div>
     </section>
-    <div class="row d-flex justify-content-center">
-        <div class="create-account" style="text-align: center;">
-            <h2 style="font-family: Osward, sans-serif;margin-top: 50px; margin-left: 10px;"><b>TẠO TÀI KHOẢN</b></h2>
-            <form method="POST" action="/register">
-                @csrf
-                <div class="mb-2 d-flex justify-content-center">
-                    <label for="username"></label>
-                    <input type="text" class="form-control" placeholder="Tạo username" name="username"
-                    style="outline: none; min-width: 150px; width: 550px;" value="{{ old('username') }}">
-                </div> <br>
-                @error('username')
-                    <p style="color: red;">{{$message}}</p>
-                @enderror
-
-                <div class="mb-4 d-flex justify-content-center">
-                    <label for="name"></label>
-                    <input type="text" class="form-control" placeholder="Tên của bạn" name="name"
-                    style="width: 550px;" value="{{ old('name') }}">
-                </div>
-                @error('name')
-                    <p style="color: red;">{{$message}}</p>
-                @enderror
-
-                <div class="mb-4 d-flex justify-content-center">
-                    <label for="email"></label>
-                    <input type="email" class="form-control" placeholder="Nhập email" name="email"
-                    style="outline: none; min-width: 150px; width: 550px;" value="{{ old('email') }}">
-                </div>
-                @error('email')
-                    <p style="color: red;">{{$message}}</p>
-                @enderror
-
-                <div class="mb-4 d-flex justify-content-center">
-                    <label for="phone"></label>
-                    <input type="tel" class="form-control" placeholder="Số điện thoại" name="phone"
-                    style="outline: none; min-width: 150px; width: 550px;" value="{{ old('phone') }}">
-                </div>
-                @error('phone')
-                    <p style="color: red;">{{$message}}</p>
-                @enderror
-
-                <div class="mb-4 d-flex justify-content-center">
-                    <label for="password"></label>
-                    <input type="password" class="form-control" placeholder="Mật khẩu" name="password"
-                    style="width: 550px;" value="{{ old('password') }}">
-                </div>
-                @error('password')
-                    <p style="color: red;">{{$message}}</p>
-                @enderror
-
-                <div class="mb-4 d-flex justify-content-center">
-                    <label for="password_confirmation"></label>
-                    <input type="password" class="form-control" placeholder="Nhập lại mật khẩu" name="password_confirmation"
-                    style="width: 550px;" value="{{ old('password_confirmation') }}">
-                </div>
-                @error('password_confirmation')
-                    <p style="color: red;">{{$message}}</p>
-                @enderror
-                <div class="policy d-flex justify-content-center" style="margin-left: 50px; margin-right: 50px;">
-                    <p style="font-family: Arial, Helvetica, sans-serif; width: 450px; padding: auto 30px">
-                        Bằng việc đăng kí, bạn đã đồng ý
-                        các <a href="#">Chính sách hoạt động</a> và <a href="#">Chính sách bảo mật</a> của Macadonuts
-                    </p>
-                </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <div class="button-signup" style="width: 550px;">
-                        <button type="submit" class="btn btn-danger btn-lg"
-                            style=" width: 300px; border-radius: 30px;">Đăng ký</button>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="create-account" style="text-align: center;">
+                <h2 style="font-family: Osward, sans-serif;margin-top: 50px; margin-left: 10px;"><b>TẠO TÀI KHOẢN</b></h2>
+                <form method="POST" action="/register">
+                    @csrf
+                    <div class="mb-2 d-flex justify-content-center">
+                        <label for="username"></label>
+                        <input type="text" class="form-control" placeholder="Tạo username" name="username"
+                        style="outline: none; min-width: 150px; width: 550px;" value="{{ old('username') }}">
+                    </div> <br>
+                    @error('username')
+                        <p style="color: red;">{{$message}}</p>
+                    @enderror
+    
+                    <div class="mb-4 d-flex justify-content-center">
+                        <label for="name"></label>
+                        <input type="text" class="form-control" placeholder="Tên của bạn" name="name"
+                        style="width: 550px;" value="{{ old('name') }}">
                     </div>
-                </div>
-                <div class="mb-3 d-flex justify-content-center">
-                    <div class="note-login d-flex justify-content-center" style="width: 550px;">
-                        <p>
-                            Bạn đã có tài khoản?<b> <a href="/login" style="text-decoration: none; color: rgb(57, 57, 57);">Đăng nhập</a> </b>
+                    @error('name')
+                        <p style="color: red;">{{$message}}</p>
+                    @enderror
+    
+                    <div class="mb-4 d-flex justify-content-center">
+                        <label for="email"></label>
+                        <input type="email" class="form-control" placeholder="Nhập email" name="email"
+                        style="outline: none; min-width: 150px; width: 550px;" value="{{ old('email') }}">
+                    </div>
+                    @error('email')
+                        <p style="color: red;">{{$message}}</p>
+                    @enderror
+    
+                    <div class="mb-4 d-flex justify-content-center">
+                        <label for="phone"></label>
+                        <input type="tel" class="form-control" placeholder="Số điện thoại" name="phone"
+                        style="outline: none; min-width: 150px; width: 550px;" value="{{ old('phone') }}">
+                    </div>
+                    @error('phone')
+                        <p style="color: red;">{{$message}}</p>
+                    @enderror
+    
+                    <div class="mb-4 d-flex justify-content-center">
+                        <label for="password"></label>
+                        <input type="password" class="form-control" placeholder="Mật khẩu" name="password"
+                        style="width: 550px;" value="{{ old('password') }}">
+                    </div>
+                    @error('password')
+                        <p style="color: red;">{{$message}}</p>
+                    @enderror
+    
+                    <div class="mb-4 d-flex justify-content-center">
+                        <label for="password_confirmation"></label>
+                        <input type="password" class="form-control" placeholder="Nhập lại mật khẩu" name="password_confirmation"
+                        style="width: 550px;" value="{{ old('password_confirmation') }}">
+                    </div>
+                    @error('password_confirmation')
+                        <p style="color: red;">{{$message}}</p>
+                    @enderror
+                    <div class="policy d-flex justify-content-center" style="margin-left: 50px; margin-right: 50px;">
+                        <p style="font-family: Arial, Helvetica, sans-serif; width: 450px; padding: auto 30px">
+                            Bằng việc đăng kí, bạn đã đồng ý
+                            các <a href="#">Chính sách hoạt động</a> và <a href="#">Chính sách bảo mật</a> của Macadonuts
                         </p>
                     </div>
-                </div>
-            </form>
+                    <div class="mb-3 d-flex justify-content-center">
+                        <div class="button-signup" style="width: 550px;">
+                            <button type="submit" class="btn btn-danger btn-lg"
+                                style=" width: 300px; border-radius: 30px;">Đăng ký</button>
+                        </div>
+                    </div>
+                    <div class="mb-3 d-flex justify-content-center">
+                        <div class="note-login d-flex justify-content-center" style="width: 550px;">
+                            <p>
+                                Bạn đã có tài khoản?<b> <a href="/login" style="text-decoration: none; color: rgb(57, 57, 57);">Đăng nhập</a> </b>
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/order/addVoucher.blade.php
+++ resources/views/order/addVoucher.blade.php
@@ -76,70 +76,72 @@
         </div>
     </section>
 
-    @if (auth()->user() && auth()->user()->user_role == 1)
-        <div class="container container-modify">
-            <div class="row d-flex">
-                <div style="">
-                    <h1 style="font-family: Osward, sans-serif; margin-top: 50px; margin-left: 10px; text-align:center;"><b>Thêm mã khuyến mãi</b></h1>
-                    <br>
-                    <form method="POST" action="/voucher/store">
-                        @csrf
-                        <div class="mb-4">
-                            <label for="voucher" class="form-label">Mã giảm giá</label>
-                            <input type="text" class="form-control" name="voucher"
-                            style="outline: none; min-width: 150px; width: 550px;" value="{{ old('voucher') }}" placeholder="MAGIAMGIA001">
-                        </div>
-                        @error('voucher')
-                        <p style="color: red; margin-top: 10px;">Vui lòng nhập đầy đủ</p>
-                        @enderror
-                        <div class="mb-4">
-                            <label for="percent" class="form-label">Phần trăm giảm</label>
-                            <input type="text" class="form-control" name="percent"
-                            style="width: 550px;" value="{{ old('percent') }}" placeholder="10%, 20%,...">
-                        </div>
-                        @error('percent')
-                        <p style="color: red; margin-top: 10px;">Vui lòng nhập đầy đủ</p>
-                        @enderror
-                        <div class="mb-4">
-                            <label for="min_bill" class="form-label">Hoá đơn tối thiểu</label>
-                            <input type="text" class="form-control" name="min_bill"
-                            style="outline: none; min-width: 150px; width: 550px;" value="{{ old('min_bill') }}" placeholder="39000, 59000,...">
-                        </div>
-                        @error('min_bill')
-                        <p style="color: red; margin-top: 10px;">Vui lòng nhập đầy đủ</p>
-                        @enderror
-                        <div class="mb-3 d-flex justify-content-center">
-                            <div class="button-signup d-flex justify-content-center">
-                                <button type="submit" class="btn btn-danger btn-lg"
-                                    style="padding: 8px 80px; border-radius: 30px;">Lưu thay đổi</button>
+    <div class="container">
+        @if (auth()->user() && auth()->user()->user_role == 1)
+            <div class="container container-modify">
+                <div class="row d-flex">
+                    <div style="">
+                        <h1 style="font-family: Osward, sans-serif; margin-top: 50px; margin-left: 10px; text-align:center;"><b>Thêm mã khuyến mãi</b></h1>
+                        <br>
+                        <form method="POST" action="/voucher/store">
+                            @csrf
+                            <div class="mb-4">
+                                <label for="voucher" class="form-label">Mã giảm giá</label>
+                                <input type="text" class="form-control" name="voucher"
+                                style="outline: none; min-width: 150px; width: 550px;" value="{{ old('voucher') }}" placeholder="MAGIAMGIA001">
                             </div>
-                        </div>
-                    </form>
+                            @error('voucher')
+                            <p style="color: red; margin-top: 10px;">Vui lòng nhập đầy đủ</p>
+                            @enderror
+                            <div class="mb-4">
+                                <label for="percent" class="form-label">Phần trăm giảm</label>
+                                <input type="text" class="form-control" name="percent"
+                                style="width: 550px;" value="{{ old('percent') }}" placeholder="10%, 20%,...">
+                            </div>
+                            @error('percent')
+                            <p style="color: red; margin-top: 10px;">Vui lòng nhập đầy đủ</p>
+                            @enderror
+                            <div class="mb-4">
+                                <label for="min_bill" class="form-label">Hoá đơn tối thiểu</label>
+                                <input type="text" class="form-control" name="min_bill"
+                                style="outline: none; min-width: 150px; width: 550px;" value="{{ old('min_bill') }}" placeholder="39000, 59000,...">
+                            </div>
+                            @error('min_bill')
+                            <p style="color: red; margin-top: 10px;">Vui lòng nhập đầy đủ</p>
+                            @enderror
+                            <div class="mb-3 d-flex justify-content-center">
+                                <div class="button-signup d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-danger btn-lg"
+                                        style="padding: 8px 80px; border-radius: 30px;">Lưu thay đổi</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endif
-    @if (isset($vouchers))
-        @foreach ($vouchers as $v)
-        <div class="row mb-3">
-            <div class="container container-modify">
-                <div>
-                    <div class="card sale-off-card  border-0">
-                        <div class="card-body">
-                            <div class="khuyen-mai-hb"> 
-                                <span class="tieu-de"><i class="icon-gift"></i> Mã khuyến mãi</span> 
-                                <ul>
-                                    <li><b>{{ strtoupper($v->getAttributes()['voucher_code']) }}</b></li>
-                                    <li>Giảm {{ $v->getAttributes()['discount_percentage'] * 100 }}%. Đơn tối thiểu {{ number_format($v->getAttributes()['min_order'], 0, ',', '.') }}đ</li>
-                                </ul>
+        @endif
+        @if (isset($vouchers))
+            @foreach ($vouchers as $v)
+            <div class="row mb-3">
+                <div class="container container-modify">
+                    <div>
+                        <div class="card sale-off-card  border-0">
+                            <div class="card-body">
+                                <div class="khuyen-mai-hb"> 
+                                    <span class="tieu-de"><i class="icon-gift"></i> Mã khuyến mãi</span> 
+                                    <ul>
+                                        <li><b>{{ strtoupper($v->getAttributes()['voucher_code']) }}</b></li>
+                                        <li>Giảm {{ $v->getAttributes()['discount_percentage'] * 100 }}%. Đơn tối thiểu {{ number_format($v->getAttributes()['min_order'], 0, ',', '.') }}đ</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        @endforeach
-    @else
-        <h1 style="font-family: Osward, sans-serif; margin-top: 50px; margin-left: 10px; text-align:center;"><b>Hiện tại không có mã giảm giá</b></h1>
-    @endif
+            @endforeach
+        @else
+            <h1 style="font-family: Osward, sans-serif; margin-top: 50px; margin-left: 10px; text-align:center;"><b>Hiện tại không có mã giảm giá</b></h1>
+        @endif
+    </div>
 @endsection
